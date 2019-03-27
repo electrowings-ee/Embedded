75f473c38691624e509ab51df8f9e760c0c178dc
Vinoth <47408971+powervinoth@users.noreply.github.com>
2019-03-28 01:04:40 +0530

    LDR with Arduino
    Interfacing LDR with Arduino to sense light intensity.

---
+++ LDR/ldr_arduino_code.ino
@@ -0,0 +1,35 @@
+const int ledPin = 13;
+
+const int ldrPin = A0;
+
+void setup() {
+
+Serial.begin(9600);
+
+pinMode(ledPin, OUTPUT);
+
+pinMode(ldrPin, INPUT);
+
+}
+
+void loop() {
+
+int ldrStatus = analogRead(ldrPin);
+
+if (ldrStatus <=300) {
+
+digitalWrite(ledPin, HIGH);
+
+Serial.println("LDR is DARK, LED is ON");
+
+}
+
+else {
+
+digitalWrite(ledPin, LOW);
+
+Serial.println("---------------");
+
+}
+delay(500);
+}
